a3dace7974c8385aab324ebcbf77571cf58d7c60
chuxiangsu <chu-xiang.su@polymtl.ca>
2025-06-13 21:38:26 -0400

    first

--- main.py
+++
@@ -1,9 +0,0 @@
-import requests
-from PIL import Image
-from io import BytesIO
-
-url = "https://static.wikia.nocookie.net/leagueoflegends/images/d/d6/Azir_Render.png/revision/latest?cb=20231105071801"
-
-response = requests.get(url)
-img = Image.open(BytesIO(response.content))
-img.show()
